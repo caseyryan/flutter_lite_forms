c619326e427e900d6b2911f7294d93b91e605fb6
Konstantin Serov <konstantin.p.serov@gmail.com>
2023-09-05 11:29:51 +0700

    initial commit

---
+++ example/lib/lite_forms_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LiteFormsPage extends StatelessWidget {
+  const LiteFormsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
