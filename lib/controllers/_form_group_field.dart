f760ae95dcb722a8fe5ca3b5585ebeb65e915094
Konstantin Serov <konstantin.p.serov@gmail.com>
2024-09-29 14:46:56 +0700

    updated deps

--- lib/controllers/_form_group_field.dart
+++ lib/controllers/_form_group_field.dart
@@ -255,7 +255,10 @@ class FormGroupField<T> {
       case AutovalidateMode.always:
       case AutovalidateMode.onUserInteraction:
         return true;
+      case AutovalidateMode.onUnfocus:
+        break;
     }
+    return false;
   }
 
   Future _checkError() async {
