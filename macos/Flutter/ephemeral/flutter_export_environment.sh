0f6e9faa3862ea19cc38183cb87a133eb4c9b2f7
Konstantin Serov <konstantin.p.serov@gmail.com>
2025-01-28 14:11:20 +0700

    return null from drop selector on cancel

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/konstantin/fvm/versions/3.7.12"
+export "FLUTTER_ROOT=/Users/konstantin/fvm/versions/3.24.3"
 export "FLUTTER_APPLICATION_PATH=/Users/konstantin/FlutterProjects/lite_forms/lite_forms"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=0.0.3"
-export "FLUTTER_BUILD_NUMBER=0.0.3"
+export "FLUTTER_BUILD_NAME=0.0.22"
+export "FLUTTER_BUILD_NUMBER=0.0.22"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
