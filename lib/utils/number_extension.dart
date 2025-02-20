09b884d763a6be3e7daa00c7581da35b4552f94c
Konstantin Serov <konstantin.p.serov@gmail.com>
2025-02-20 15:22:32 +0700

    number extension

--- lib/utils/number_extension.dart
+++ lib/utils/number_extension.dart
@@ -1,4 +1,12 @@
 extension IntExtension on int {
+
+  String toCounterLabel() {
+    if (this > 9) {
+      return '9+';
+    }
+    return toString();
+  }
+
   String kilobytesToLabel() {
     final megaBytes = this ~/ 1024;
     final leftOverKilobytes = this % 1024;
