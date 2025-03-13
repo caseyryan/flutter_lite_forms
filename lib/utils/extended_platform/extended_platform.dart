7d351c2d65f5d9408d23c1c43b4d149a09bd3f79
Konstantin Serov <konstantin.p.serov@gmail.com>
2025-03-13 13:34:19 +0700

    update extended platform

--- lib/utils/extended_platform/extended_platform.dart
+++ lib/utils/extended_platform/extended_platform.dart
@@ -5,13 +5,17 @@ import 'package:flutter/foundation.dart';
 import 'os_stub.dart' if (dart.library.html) 'web_stub.dart';
 
 class ExtendedPlatform {
-  static bool get isIOS {
+  static bool get isWebIOS {
     if (kIsWeb) {
       final userAgentLower = window.navigator.userAgent.toLowerCase();
       return userAgentLower.contains('ios') ||
           userAgentLower.contains('iphone') ||
           userAgentLower.contains('ipad');
     }
+    return false;
+  }
+
+  static bool get isIOS {
     return Platform.isIOS;
   }
 }
